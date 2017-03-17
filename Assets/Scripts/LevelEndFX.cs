3138d3a79ef57b94a6c76fd01664d98be8c47ae6
valgoun <valgoun@gmail.com>
2017-03-17 15:47:24 +0100

    Collectible ; Spikes and LevelEnd

---
+++ Assets/Scripts/LevelEndFX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class LevelEndFX : MonoBehaviour
+{
+    private Transform sphere1;
+    private Transform sphere2;
+    private Transform sphere3;
+
+    // Use this for initialization
+    void Start()
+    {
+        sphere1 = transform.GetChild(0);
+        sphere2 = transform.GetChild(1);
+        sphere3 = transform.GetChild(2);
+
+        sphere1.DOLocalRotate(new Vector3(0, 0, -180), 1.1f).SetLoops(-1, LoopType.Incremental).SetEase(Ease.Linear);
+        sphere2.DOLocalRotate(new Vector3(0, 0, -180), 1.2f).SetLoops(-1, LoopType.Incremental).SetEase(Ease.Linear);
+        sphere3.DOLocalRotate(new Vector3(0, 0, -180), 1.3f).SetLoops(-1, LoopType.Incremental).SetEase(Ease.Linear);
+    }
+}
